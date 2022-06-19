de17b2ddf2d6180caee127edf3564aa69894bc9b
Supriya090 <khadkasupriya76@gmail.com>
2022-06-19 23:50:26 +0545

    User Interface Initiated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import useStyles from './styles/Home'
+import taxiImage from "../assets/taxi.png"
 
 const Home = () => {
     const classes = useStyles();
     return (
-        <div className={classes.mainContent}>Share Your Ride</div>
+        <div className={classes.mainContent}>Share Your Ride
+            <img src={taxiImage} className={classes.taxiImageStyle} alt="Taxi" />
+        </div>
     )
 }
 
--- src/components/styles/Home.js
+++ src/components/styles/Home.js
@@ -1,7 +1,27 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
+    mainContent: {
+        width: "55vw",
+        height: "80vh",
+        backgroundColor: "#fff",
+        margin: "auto",
+        margiinTop: "20px",
+        borderRadius: "15px",
+        position: "relative",
+        fontFamily: "Nunito, sans- serif",
+        fontWeight: 800,
+        fontSize: "2rem",
+        padding: "30px 40px"
+    },
 
+    taxiImageStyle: {
+        width: "400px",
+        height: "275px",
+        position: "absolute",
+        top: 0,
+        right: 0
+    }
 }))
 
 export default useStyles;
