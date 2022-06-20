5dc91852323a61bd5bc4ff8eeaf65e0628011bbf
Supriya090 <khadkasupriya76@gmail.com>
2022-06-20 13:33:03 +0545

    Text Fields Added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,50 @@
 import React from 'react'
 import useStyles from './styles/Home'
 import taxiImage from "../assets/taxi.png"
+import taxiIllustration from "../assets/CarIllustration.png"
+import { Button, TextField, Typography } from '@material-ui/core';
 
 const Home = () => {
     const classes = useStyles();
     return (
-        <div className={classes.mainContent}>Share Your Ride
-            <img src={taxiImage} className={classes.taxiImageStyle} alt="Taxi" />
+        <div className={classes.mainContent}>
+            <Button variant='contained' className={classes.connectButton}>Connect Wallet</Button>
+            <Typography className={classes.addressText}>
+                Address:
+            </Typography>
+            <Typography className={classes.addressText}>
+                Balance:
+            </Typography>
+            <Typography className={classes.titleText}>RideSaathi</Typography>
+            <img src={taxiImage} className={classes.taxiImageStyle} alt="TaxiImage" />
+            <div className={classes.rideGraphicsDiv}>
+                <img src={taxiIllustration} className={classes.taxiIllustrationStyle} alt="TaxiIllustration" />
+                <div className={classes.rideInfoDiv}>
+                    <form className={classes.userForm}>
+                        <div className={classes.textFields}>
+                            <TextField
+                                id='pickupLocation'
+                                label="Enter Pickup Location"
+                                variant='outlined'
+                                name='pickupLocation'
+                                className={classes.textField}
+                            />
+                            <TextField
+                                id='dropoffLocation'
+                                label="Enter Drop Off Location"
+                                variant='outlined'
+                                name='dropoffLocation'
+                                className={classes.textField}
+                            />
+                        </div>
+                        <input
+                            type='button'
+                            value="Find a Ride"
+                            className={`${classes.connectButton} ${classes.rideButton}`} />
+                    </form>
+                </div>
+            </div>
+            {/* <Typography>Easing Lives One Ride at a Time</Typography> */}
         </div>
     )
 }
--- src/components/styles/Home.js
+++ src/components/styles/Home.js
@@ -9,10 +9,19 @@ const useStyles = makeStyles((theme) => ({
         margiinTop: "20px",
         borderRadius: "15px",
         position: "relative",
+        padding: "30px 40px",
+
+    },
+
+    addressText: {
+        fontFamily: "Inter, sans-serif",
+        fontSize: "1.1rem"
+    },
+    titleText: {
         fontFamily: "Nunito, sans- serif",
         fontWeight: 800,
-        fontSize: "2rem",
-        padding: "30px 40px"
+        fontSize: "2.5rem",
+        marginTop: "10px"
     },
 
     taxiImageStyle: {
@@ -21,6 +30,74 @@ const useStyles = makeStyles((theme) => ({
         position: "absolute",
         top: 0,
         right: 0
+    },
+
+    connectButton: {
+        backgroundColor: "#000",
+        color: "#fff",
+        fontFamily: "Nunito, sans- serif",
+        fontWeight: 800,
+        fontSize: "1rem",
+        padding: "7px 15px 5px 15px",
+        marginBottom: "20px",
+        borderRadius: "7px",
+        transition: "0.3s all ease-out",
+        "&:hover": {
+            backgroundColor: "rgba(0, 0, 0,0.8)",
+        },
+    },
+
+    rideInfoDiv: {
+        backgroundColor: "#E5E5E5",
+        width: "45%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "30px 0px",
+        borderRadius: "10px",
+        marginTop: "20px"
+    },
+
+    taxiIllustrationStyle: {
+        position: "absolute",
+        bottom: 15,
+        right: 100,
+        height: "300px",
+        width: "260px"
+    },
+    userForm: {
+        '& label.Mui-focused': {
+            color: '#000',
+        },
+        '& .MuiOutlinedInput-root': {
+            backgroundColor: "#D9D9D9",
+            '& fieldset': {
+                //borderColor: '#fff',
+                //backgroundColor: "#D9D9D9",
+                //color: "#000"
+            },
+            '&:hover fieldset': {
+                borderColor: '#000',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#000',
+                color: "#000",
+
+            },
+        },
+    },
+    textFields: {
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        marginTop: "10px"
+    },
+    textField: {
+        marginBottom: "15px"
+    },
+
+    rideButton: {
+        marginBottom: "0px"
     }
 }))
 
