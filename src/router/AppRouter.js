4dd600cfefc0e7c496ebd258d158d951e21635b0
Rahul Shah <075bct063.rahul@pcampus.edu.np>
2022-07-01 03:58:22 +0545

    loaction

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import '../App.css';
 import Home from '../components/Home';
+import Location from '../components/Location';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/location" element={<Location />}></Route>
       </Routes>
     </div>
   );
